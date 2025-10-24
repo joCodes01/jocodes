b5660a8b4d93c8c2422555b57cf9b472c1763357
joCodes <76087277+joCodes01@users.noreply.github.com>
2025-10-24 15:43:50 +1100

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+export function Navbar() {
+    return(
+        <nav className='navbar'>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/contact'>Contact</NavLink>
+        </nav>
+    )
+}
+
+export default Navbar
